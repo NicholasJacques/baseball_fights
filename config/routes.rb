84f256bb4edef3cf69737466d84324a65f525d59
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-06 23:21:03 -0600

    teams index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :teams, only: [:index]
+
   resources :fights, only: [:new, :create, :show, :index, :edit, :update]
 end
--- spec/features/teams/can_view_all_teams_spec.rb
+++ spec/features/teams/can_view_all_teams_spec.rb
@@ -1,2 +1,14 @@
 require 'rails_helper'
 
+RSpec.describe "teams inded" do
+  context "as a user" do
+    it "can view teams inded" do
+      team_1, team_2 = create_list(:team, 2)
+
+      visit teams_path
+
+      expect(page).to have_content(team_1.name)
+      expect(page).to have_content(team_2.name)
+    end
+  end
+end
